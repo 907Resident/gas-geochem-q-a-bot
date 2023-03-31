739d17bde1f5185a6da8f76678ad12a79dd8336e
907Resident <907Resident@users.noreply.github.com>
2023-03-30 23:33:48 -0600

    updated the scrape toy script
    the toy script can now scrape the text from a given .pdf and export that text to a .txt file. this makes it easier to use with langchain.
    
    the next step will be to formalize and clean up code as a function or series of functions
    
    affiliated with issue #1

--- programs/src/utils/scrape_pdf_text.py
+++ programs/src/utils/scrape_pdf_text.py
@@ -1,5 +1,8 @@
 # %% import necessary packages
 
+# unstructured // open source package to manipulate files containing unstructured data
+from unstructured.partition.auto import partition
+
 # PyPDF2 // open source package to manipulate .pdf files
 from PyPDF2 import PdfReader
 
@@ -9,10 +12,14 @@ import pdfplumber
 # essential packages
 import os
 
+# TODO: Overall, this needs to be converted to one or a few functions
+
 # %% upload necessary .pdf file
 
 # pdf 
 lowenstern_pdf_path = "../../../data/yellowstone/Lowenstern_Origins_Geothermal_Gases_at_Yellowstone.pdf"
+
+out_txt = "../../../data/yellowstone/Lowenstern_Orrigins_Geothermal_Gases_at_Yellowstone.txt"
 
 # %% pdfplumber
 
@@ -30,4 +37,14 @@ pagehandle = py_pdfreader.getPage(1)
 
 print(pagehandle.extractText())
 
+# %% unstructured
+
+elements = partition(lowenstern_pdf_path)
+
+with open(out_txt, "w") as f:
+    for elem in elements:
+        f.write("{}\n\n".format(elem))
+
+
+print("\n\n".join([str(el) for el in elements]))
 # %%
